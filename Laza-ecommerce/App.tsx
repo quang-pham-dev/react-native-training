81ed42d76e45c26da8db72562d3c4d917a1b94f1
quangpham-agilityio <quang.pham@asnet.com.vn>
2022-05-06 12:56:24 +0700

    add google font Inter

--- Laza-ecommerce/App.tsx
+++ Laza-ecommerce/App.tsx
@@ -2,8 +2,36 @@
 import * as React from 'react';
 import RootNavigation from 'navigation/RootNavigator';
 import 'react-native-gesture-handler';
+import {
+  useFonts,
+  Inter_100Thin,
+  Inter_200ExtraLight,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  Inter_900Black,
+} from '@expo-google-fonts/inter';
+import LoadingIndicator from 'components/Indicator';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_100Thin,
+    Inter_200ExtraLight,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+    Inter_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return <LoadingIndicator loadingSize={'large'} />;
+  }
   return (
     <>
       <RootNavigation />
