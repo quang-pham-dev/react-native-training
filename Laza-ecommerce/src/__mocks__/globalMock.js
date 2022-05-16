39d72947139916d717b91a7cdb1be78ec1e0cd9d
quangpham-agilityio <quang.pham@asnet.com.vn>
2022-05-16 17:39:04 +0700

    updated mock json server.

--- Laza-ecommerce/src/__mocks__/globalMock.js
+++ Laza-ecommerce/src/__mocks__/globalMock.js
@@ -1,3 +1,7 @@
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+import '@testing-library/jest-dom';
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+
+global.window = {};
+global.window = global;
