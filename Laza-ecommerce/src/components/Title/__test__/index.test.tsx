2215cb6f1fb7898162f414d75d30b563f45bdb1a
quangpham-agilityio <quang.pham@asnet.com.vn>
2022-05-17 19:29:41 +0700

    update testing result

--- Laza-ecommerce/src/components/Title/__test__/index.test.tsx
+++ Laza-ecommerce/src/components/Title/__test__/index.test.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Title from 'components/Title';
-import { Colors, Fonts } from 'styles/themes';
 
 describe('Title Component', () => {
   test('should render  correctly', () => {
-    const tree = renderer
-      .create(
-        <Title
-          titleName='Welcome'
-          fontSize={Fonts.size.h5}
-          fontFamily={Fonts.fontFamily.Inter_500Medium}
-          color={Colors.textBlack}
-        ></Title>,
-      )
-      .toJSON();
+    const tree = renderer.create(<Title titleName='Welcome'></Title>).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
