3f54ad82e8a3e0956e36a11579c4fe41463d4858
quangpham-agilityio <quang.pham@asnet.com.vn>
2022-05-17 10:20:12 +0700

    update feature authentication

--- Laza-ecommerce/src/context/AppContext.tsx
+++ Laza-ecommerce/src/context/AppContext.tsx
@@ -1,48 +1,16 @@
 import React, { createContext, useReducer } from 'react';
 // Context Reducer
 import { authReducer } from './Reducers/AuthReducer';
-import { productReducer } from './Reducers/ProductReducer';
 // Context initial state
-import { AuthState, InitialAuthState } from './State/AuthState';
-import { InitialProductState, ProductState } from './State/ProductState';
-
-// export const combineReducers = (reducers: any) => {
-//   return (state: any, action: any) => {
-//     const temporaryState = { ...state };
-//     Object.keys(reducers).forEach(key => {
-//       temporaryState[key] = reducers[key](temporaryState[key], action);
-//     });
-
-//     return temporaryState;
-//   };
-// };
-
-// interface StateApp {
-//   auth: AuthState;
-//   product: ProductState;
-// }
-
-// export const initialStateApp: StateApp = {
-//   auth: InitialAuthState,
-//   product: InitialProductState,
-// };
+import { InitialAuthState } from './State/AuthState';
 
 export const AppContext = createContext<any>({});
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  //   const rootReducer = combineReducers({
-  //     auth: authReducer,
-  //     products: productReducer,
-  //   });
 
   const [authState, authDispatch] = useReducer(authReducer, InitialAuthState);
-  const [productState, productDispatch] = useReducer(productReducer, InitialProductState);
 
-  return (
-    <AppContext.Provider value={{ authState, authDispatch, productState, productDispatch }}>
-      {children}
-    </AppContext.Provider>
-  );
+  return <AppContext.Provider value={{ authState, authDispatch }}>{children}</AppContext.Provider>;
 };
 
 export default AuthProvider;
