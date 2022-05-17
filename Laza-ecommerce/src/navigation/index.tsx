3af3b3323e0f3e10bc208f2e7659f0bdb088fb57
quangpham-agilityio <quang.pham@asnet.com.vn>
2022-05-17 23:27:22 +0700

    fix render two time when logout

--- Laza-ecommerce/src/navigation/index.tsx
+++ Laza-ecommerce/src/navigation/index.tsx
@@ -9,13 +9,14 @@ import { AppContext } from 'context/AppContext';
 // Types
 import { INITIALIZE } from 'types/Actions';
 // Constants
-import { AuthData } from 'constants/Common';
+import { AUTH_DATA } from 'constants/Common';
 
 const RootNavigator: React.FC = () => {
   // get status authenticated from context
   const { authState, authDispatch } = useContext(AppContext);
+  // check if user is authenticated
   const getAuth = async () => {
-    const authData = await AsyncStorage.getItem(AuthData);
+    const authData = await AsyncStorage.getItem(AUTH_DATA);
     if (authData) {
       const { user } = JSON.parse(authData);
       const { access_token } = JSON.parse(authData);
@@ -26,16 +27,22 @@ const RootNavigator: React.FC = () => {
     }
   };
 
-  console.log('authState', authState);
-
   useEffect(() => {
     getAuth();
   }, []);
-
   return (
-    <NavigationContainer>
-      {authState?.isAuthenticated ? <AppDrawerNavigator /> : <AuthNavigator />}
-    </NavigationContainer>
+    <>
+      {authState?.isAuthenticated && (
+        <NavigationContainer>
+          <AppDrawerNavigator />
+        </NavigationContainer>
+      )}
+      {!authState.isAuthenticated && (
+        <NavigationContainer>
+          <AuthNavigator />
+        </NavigationContainer>
+      )}
+    </>
   );
 };
 export default RootNavigator;
