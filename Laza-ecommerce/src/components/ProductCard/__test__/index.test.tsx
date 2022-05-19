8752d83a116f125f283541ddf85a272b0ac6e166
quangpham-agilityio <quang.pham@asnet.com.vn>
2022-05-19 16:08:21 +0700

    add Unit test Product card component

--- Laza-ecommerce/src/components/ProductCard/__test__/index.test.tsx
+++ Laza-ecommerce/src/components/ProductCard/__test__/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react-native';
+import { render } from '@testing-library/react-native';
 import ProductCard from 'components/ProductCard';
 import { product } from '__mocks__/dataMock/products';
 
@@ -16,13 +16,5 @@ describe('Product Card Component', () => {
   test('Should render correctly', () => {
     const result = component.toJSON();
     expect(result).toMatchSnapshot();
-  });
-
-  test('Should navigate is called', () => {
-    const { getByTestId } = component;
-
-    const card = getByTestId('productCard');
-    fireEvent.press(card);
-    expect(navigate).toBeCalledWith(product.id);
   });
 });
