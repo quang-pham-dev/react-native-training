6ce4fb5bd95bdbdd3759c35b2f0cb0cddac2231c
quangpham-agilityio <quang.pham@asnet.com.vn>
2022-05-19 16:11:54 +0700

    add Unit test search bar component

---
+++ Laza-ecommerce/src/components/SearchBar/__test__/index.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import SearchBar from 'components/SearchBar';
+
+describe('Search Bar Component', () => {
+  const component = render(<SearchBar onChangeText={jest.fn()} />);
+  test('Should render correctly', () => {
+    const result = component.toJSON();
+    expect(result).toMatchSnapshot();
+  });
+});
