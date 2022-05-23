388255445bfc329ec6e4052f50f9d966475b4ffd
quangpham-agilityio <quang.pham@asnet.com.vn>
2022-05-23 22:33:11 +0700

    feature/implement-navigation-app

--- Laza-ecommerce/src/navigation/RootNavigator.tsx
+++ Laza-ecommerce/src/navigation/RootNavigator.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Navigator
 import AuthStack from './stacks/AuthStack';
@@ -14,10 +15,15 @@ import LoadingIndicator from 'components/LoadingIndicator';
 
 // Constants
 import { AUTH_DATA } from 'constants/Common';
+import { SCREENS_ROUTES } from 'constants/Screens';
+
+// Types
+import { LOADING_SIZE } from 'types/common/Enums';
 
 // Utils
 import { get } from 'utils/localStorage';
-import { LOADING_SIZE } from 'types/common/Enums';
+
+const RootStack = createNativeStackNavigator();
 
 const RootNavigator: React.FC = () => {
   // get status authenticated from context
@@ -49,9 +55,18 @@ const RootNavigator: React.FC = () => {
     getAuth();
   }, [authDispatch]);
 
+  const renderStack = () => {
+    if (isAuthenticated) {
+      return <RootStack.Screen component={AppDrawerStack} name={SCREENS_ROUTES.STACK.APP.name} />;
+    }
+    return <RootStack.Screen component={AuthStack} name={SCREENS_ROUTES.STACK.AUTH.name} />;
+  };
+
   return (
     <NavigationContainer fallback={<LoadingIndicator loadingSize={LOADING_SIZE.LARGE} />}>
-      {isAuthenticated ? <AppDrawerStack /> : <AuthStack />}
+      <RootStack.Navigator screenOptions={{ headerShown: false }}>
+        {renderStack()}
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 };
