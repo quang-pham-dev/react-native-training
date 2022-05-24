d4230b0827689e02c7e4e1d5e9cb9bc3103f8895
quangpham-agilityio <quang.pham@asnet.com.vn>
2022-05-24 13:46:14 +0700

    format code.

--- Laza-ecommerce/src/screens/ProductDetail/index.tsx
+++ Laza-ecommerce/src/screens/ProductDetail/index.tsx
@@ -6,7 +6,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 // Components
 import Button from 'components/Button';
 import MoreLessText from 'components/LessMoreText';
-import ProductSize from 'components/ProductDetail/ProductSize/ProductSize';
+import ProductSize from 'components/ProductSize/ProductSize';
 import Title from 'components/Title';
 import LoadingIndicator from 'components/LoadingIndicator';
 
@@ -66,160 +66,151 @@ const ProductDetail = ({ navigation, route }: IProductDetailProps) => {
   // handle action press button cart
   const handlePressCart = () => {};
 
-  {
-    productById?.isProcessing && <LoadingIndicator />;
-  }
   return (
     <View style={styles.Container}>
-      {isProcessing ? (
-        <SafeAreaView>
-          <LoadingIndicator loadingSize={LOADING_SIZE.LARGE} />
-        </SafeAreaView>
-      ) : (
-        <>
-          <View style={styles.actionsWrapper}>
-            <Pressable onPress={handlePressBack}>
-              <Image style={styles.backIcon} source={IMAGES.iconBack} />
-            </Pressable>
-            <Pressable onPress={handlePressCart}>
-              <Image style={styles.cartIcon} source={IMAGES.iconCart} />
-            </Pressable>
+      <View style={styles.actionsWrapper}>
+        <Pressable onPress={handlePressBack}>
+          <Image style={styles.backIcon} source={IMAGES.iconBack} />
+        </Pressable>
+        <Pressable onPress={handlePressCart}>
+          <Image style={styles.cartIcon} source={IMAGES.iconCart} />
+        </Pressable>
+      </View>
+      <ScrollView style={styles.mainContainer} showsVerticalScrollIndicator={false}>
+        <View style={styles.headerContainer}>
+          <View style={styles.headerImageWrapper}>
+            {isProcessing
+              ? null
+              : productById && (
+                  <ImageBackground style={styles.image} source={{ uri: productById[0]?.source }} />
+                )}
           </View>
-          <ScrollView style={styles.mainContainer} showsVerticalScrollIndicator={false}>
-            <View style={styles.headerContainer}>
-              <View style={styles.headerImageWrapper}>
-                {productById && (
-                  <ImageBackground style={styles.image} source={{ uri: productById[0].source }} />
-                )}
-              </View>
+        </View>
+
+        <View style={styles.contentContainer}>
+          <View style={styles.productInfoContainer}>
+            <View style={styles.productInfoWrapper}>
+              {productById && (
+                <>
+                  <Title titleName={productById[0]?.title} titleStyles={styles.textTitle} />
+                  <Title titleName={productById[0]?.type} titleStyles={styles.textValue} />
+                </>
+              )}
             </View>
-
-            <View style={styles.contentContainer}>
-              <View style={styles.productInfoContainer}>
-                <View style={styles.productInfoWrapper}>
-                  {productById && (
-                    <>
-                      <Title titleName={productById[0].title} titleStyles={styles.textTitle} />
-                      <Title titleName={productById[0].type} titleStyles={styles.textValue} />
-                    </>
-                  )}
-                </View>
-                <View style={styles.productInfoWrapper}>
-                  <Title titleName='Price' titleStyles={styles.textTitle} />
-                  {productById && (
-                    <Title titleName={`$${productById[0].price}`} titleStyles={styles.textValue} />
-                  )}
-                </View>
-              </View>
-
-              {/* image review  */}
-              <ScrollView
-                style={styles.productImageReview}
-                horizontal
-                showsHorizontalScrollIndicator={false}>
-                {productById &&
-                  productById[0]?.imageReview?.map((item: any) => (
-                    <Pressable style={styles.productImageReviewWrapper} key={item.id}>
-                      <Image
-                        style={styles.imagePreview}
-                        source={{ uri: item.image }}
-                        resizeMode='cover'
-                      />
-                    </Pressable>
-                  ))}
-              </ScrollView>
-              {/* Product size */}
-              <ProductSize />
-
-              <View style={styles.descriptionContainer}>
-                <Title titleName='Description' titleStyles={styles.textDescriptionTitle} />
-                {productById && (
-                  <MoreLessText styleShowMoreText={styles.textContent} numberOfLines={3}>
-                    {productById[0].description}
-                  </MoreLessText>
-                )}
-              </View>
-
-              <View style={styles.reviewContainer}>
-                <View style={styles.reviewTitleWrapper}>
-                  <Title titleName='Reviews' titleStyles={styles.textReviewTitle} />
-                  <Title titleName='View All' titleStyles={styles.textTitle} />
-                </View>
-                <View style={styles.reviewWrapper}>
-                  <View style={styles.reviewItem}>
-                    <View style={styles.reviewItemWrapper}>
-                      <View style={styles.reviewerInfoWrapper}>
-                        {productById && (
-                          <Image
-                            style={styles.imageAvatar}
-                            source={{ uri: productById[0]?.reviewer?.image }}
-                          />
-                        )}
-                        <View style={styles.reviewerItemInfo}>
-                          {productById && (
-                            <Title
-                              titleName={productById[0]?.reviewer?.name}
-                              titleStyles={styles.textName}
-                            />
-                          )}
-                          <View style={styles.reviewTime}>
-                            <Image style={styles.imageClock} source={IMAGES.iconClock} />
-                            {productById && (
-                              <Text style={styles.textDate}>{productById[0]?.reviewer?.date}</Text>
-                            )}
-                          </View>
-                        </View>
-                      </View>
-                      <View style={styles.reviewItemRating}>
-                        <View style={styles.ratingTextWrapper}>
-                          {productById && (
-                            <Text style={styles.ratingTextPoint}>{productById[0].rating}</Text>
-                          )}
-                          <Text style={styles.ratingText}>rating</Text>
-                        </View>
-                        <Image style={styles.imageStar} source={IMAGES.ratingStart} />
-                      </View>
-                    </View>
-                    <View style={styles.reviewItemContent}>
-                      {productById && (
-                        <MoreLessText styleShowMoreText={styles.textContent} numberOfLines={3}>
-                          {productById[0].comment}
-                        </MoreLessText>
-                      )}
-                    </View>
-                  </View>
-                </View>
-              </View>
-
-              <View style={styles.totalPriceContainer}>
-                <View style={styles.totalPriceWrapper}>
-                  <View style={styles.textTotalWrapper}>
-                    {productById && (
-                      <>
-                        <Title titleName='Total Price' titleStyles={styles.textTotalPrice} />
-                        <Title titleName='with VAT,SD' titleStyles={styles.textVAT} />
-                      </>
-                    )}
-                  </View>
-                  {productById && (
-                    <Title titleName={`$${productById[0].price}`} titleStyles={styles.textPrice} />
-                  )}
-                </View>
-              </View>
+            <View style={styles.productInfoWrapper}>
+              <Title titleName='Price' titleStyles={styles.textTitle} />
+              {productById && (
+                <Title titleName={`$${productById[0]?.price}`} titleStyles={styles.textValue} />
+              )}
             </View>
+          </View>
 
-            <View style={styles.footerContainer}>
-              <Button
-                testID='AddtoCartButton'
-                text='Add to Cart'
-                buttonStyles={[styles.bottomButton, styles.loginButton]}
-                textStyles={[styles.textBottomButton]}
-                onPress={() => {}}
-              />
-            </View>
+          {/* image review  */}
+          <ScrollView
+            style={styles.productImageReview}
+            horizontal
+            showsHorizontalScrollIndicator={false}>
+            {productById &&
+              productById[0]?.imageReview?.map((item: any) => (
+                <Pressable style={styles.productImageReviewWrapper} key={item.id}>
+                  <Image
+                    style={styles.imagePreview}
+                    source={{ uri: item.image }}
+                    resizeMode='cover'
+                  />
+                </Pressable>
+              ))}
           </ScrollView>
-        </>
-      )}
+          {/* Product size */}
+          <ProductSize />
+
+          <View style={styles.descriptionContainer}>
+            <Title titleName='Description' titleStyles={styles.textDescriptionTitle} />
+            {productById && (
+              <MoreLessText styleShowMoreText={styles.textContent} numberOfLines={3}>
+                {productById[0]?.description}
+              </MoreLessText>
+            )}
+          </View>
+
+          <View style={styles.reviewContainer}>
+            <View style={styles.reviewTitleWrapper}>
+              <Title titleName='Reviews' titleStyles={styles.textReviewTitle} />
+              <Title titleName='View All' titleStyles={styles.textTitle} />
+            </View>
+            <View style={styles.reviewWrapper}>
+              <View style={styles.reviewItem}>
+                <View style={styles.reviewItemWrapper}>
+                  <View style={styles.reviewerInfoWrapper}>
+                    {productById && (
+                      <Image
+                        style={styles.imageAvatar}
+                        source={{ uri: productById[0]?.reviewer?.image }}
+                      />
+                    )}
+                    <View style={styles.reviewerItemInfo}>
+                      {productById && (
+                        <Title
+                          titleName={productById[0]?.reviewer?.name}
+                          titleStyles={styles.textName}
+                        />
+                      )}
+                      <View style={styles.reviewTime}>
+                        <Image style={styles.imageClock} source={IMAGES.iconClock} />
+                        {productById && (
+                          <Text style={styles.textDate}>{productById[0]?.reviewer?.date}</Text>
+                        )}
+                      </View>
+                    </View>
+                  </View>
+                  <View style={styles.reviewItemRating}>
+                    <View style={styles.ratingTextWrapper}>
+                      {productById && (
+                        <Text style={styles.ratingTextPoint}>{productById[0]?.rating}</Text>
+                      )}
+                      <Text style={styles.ratingText}>rating</Text>
+                    </View>
+                    <Image style={styles.imageStar} source={IMAGES.ratingStart} />
+                  </View>
+                </View>
+                <View style={styles.reviewItemContent}>
+                  {productById && (
+                    <MoreLessText styleShowMoreText={styles.textContent} numberOfLines={3}>
+                      {productById[0]?.comment}
+                    </MoreLessText>
+                  )}
+                </View>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.totalPriceContainer}>
+            <View style={styles.totalPriceWrapper}>
+              <View style={styles.textTotalWrapper}>
+                {productById && (
+                  <>
+                    <Title titleName='Total Price' titleStyles={styles.textTotalPrice} />
+                    <Title titleName='with VAT,SD' titleStyles={styles.textVAT} />
+                  </>
+                )}
+              </View>
+              {productById && (
+                <Title titleName={`$${productById[0]?.price}`} titleStyles={styles.textPrice} />
+              )}
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.footerContainer}>
+          <Button
+            testID='AddtoCartButton'
+            text='Add to Cart'
+            buttonStyles={[styles.bottomButton, styles.loginButton]}
+            textStyles={[styles.textBottomButton]}
+            onPress={() => {}}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
