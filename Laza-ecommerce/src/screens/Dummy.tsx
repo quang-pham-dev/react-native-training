a27f17525f9d0a071b58b9857807e266f872f811
quangpham-agilityio <quang.pham@asnet.com.vn>
2022-05-25 21:13:18 +0700

    remove logout Button.

--- Laza-ecommerce/src/screens/Dummy.tsx
+++ Laza-ecommerce/src/screens/Dummy.tsx
@@ -1,40 +1,10 @@
-import React, { useContext } from 'react';
-import { Button, Text, View, StyleSheet, Alert } from 'react-native';
-
-// Context
-import { AppContext } from 'context/AppContext';
-import { SIGN_OUT, SIGN_OUT_SUCCESS, SIGN_OUT_FAILED } from 'context/actions/auth';
-
-// API
-import { authService } from 'api';
-
-// Constants
-import { AUTH_DATA } from 'constants/Common';
-
-// Utils
-import { remove } from 'utils/localStorage';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 const DummyScreen = () => {
-  const { authDispatch } = useContext(AppContext);
-
-  const handlePressSignOut = async () => {
-    authDispatch({ type: SIGN_OUT });
-    try {
-      await authService.signOut();
-      await remove(AUTH_DATA);
-      authDispatch({
-        type: SIGN_OUT_SUCCESS,
-      });
-    } catch (error) {
-      authDispatch({ type: SIGN_OUT_FAILED, payload: error });
-      Alert.alert('Error', error.message);
-    }
-  };
-
   return (
     <View style={styles.container}>
       <Text>APP VERSION NOT YET DEVELOPED</Text>
-      <Button title='LogOut' onPress={handlePressSignOut} />
     </View>
   );
 };
