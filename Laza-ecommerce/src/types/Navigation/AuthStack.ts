803290df37a77fc74897aa2d5c3b843a05db8584
quangpham-agilityio <quang.pham@asnet.com.vn>
2022-05-25 18:06:50 +0700

    Delete conflict file.

--- Laza-ecommerce/src/types/Navigation/AuthStack.ts
+++
@@ -1,5 +0,0 @@
-export type AuthStackParams = {
-  SignIn: undefined;
-  SignUp: undefined;
-  GetStarted: undefined;
-};
--- Laza-ecommerce/src/types/Navigation/HomeStack.ts
+++
@@ -1,6 +0,0 @@
-export type HomeStackParams = {
-  Home: undefined;
-  ProductDetail: undefined;
-  BrandDetail: undefined;
-  DrawerMenu: undefined;
-};
