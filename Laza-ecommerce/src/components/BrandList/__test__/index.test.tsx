c0ea78efd6474d1332703d10844a76f05126ebca
quangpham-agilityio <quang.pham@asnet.com.vn>
2022-05-27 18:23:42 +0700

    update Unit testing.

--- Laza-ecommerce/src/components/BrandList/__test__/index.test.tsx
+++ Laza-ecommerce/src/components/BrandList/__test__/index.test.tsx
@@ -1,34 +1,48 @@
 import React from 'react';
-import BrandsCardList from 'components/BrandList';
-import { brands } from '__mocks__/dataMock';
-import renderer from 'react-test-renderer';
 import { TouchableOpacity } from 'react-native';
+
+// LIBS
+import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react-native';
+
+// Components
+import BrandsCardList from 'components/BrandList';
+
+// Mocks
+import { brands } from '__mocks__/dataMock';
+
+// Constants
 import { BRANDS_EMPTY_RESULT } from 'constants/Brands';
 
-describe('Brand Card List Component', () => {
-  const navigate = jest.fn();
+// Utils
+import { navigationMock } from 'utils/testMock';
 
-  const component = renderer.create(
-    <BrandsCardList onNavigateBrandDetailScreen={navigate} brandsData={brands} />,
-  );
+describe('Brand Card List Component', () => {
+  const props = {
+    onNavigateBrandDetailScreen: navigationMock.navigate,
+    brands,
+  };
+
+  const tree = renderer.create(<BrandsCardList {...props} />);
   test('Should render correctly', () => {
-    const result = component.toJSON();
-    expect(result).toMatchSnapshot();
+    const component = tree.toJSON();
+    expect(component).toMatchSnapshot();
   });
 
   test('should call function onNavigateBrandDetailScreen', () => {
-    const press = component.root.findAllByType(TouchableOpacity)[0];
-    press.props.onPress();
-    expect(navigate).toHaveBeenCalled();
+    const Pressable = tree.root.findAllByType(TouchableOpacity)[0];
+    Pressable.props.onPress();
+    expect(navigationMock.navigate).toHaveBeenCalled();
   });
 
-  test('Should render No Brand found text when brandsData is empty', () => {
-    const component = render(
-      <BrandsCardList onNavigateBrandDetailScreen={navigate} brandsData={[]} />,
-    );
+  test('Should render No Brand found text when brands is empty', () => {
+    const newProps = {
+      ...props,
+      brands: [],
+    };
+    const component = render(<BrandsCardList {...newProps} />);
     const { getByText } = component;
-    const label = getByText(BRANDS_EMPTY_RESULT);
-    expect(label).toBeTruthy();
+    const textContent = getByText(BRANDS_EMPTY_RESULT);
+    expect(textContent).toBeTruthy();
   });
 });
