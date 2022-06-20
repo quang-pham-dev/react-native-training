861bd4e0d3d230f12ad3d3207c16c5508afd4216
quangpham-agilityio <quang.pham@asnet.com.vn>
2022-06-20 18:44:15 +0700

    Fix: optimize images convert png to webp type

--- Laza-ecommerce/src/themes/Images.ts
+++ Laza-ecommerce/src/themes/Images.ts
@@ -1,32 +1,30 @@
 const IMAGES = {
-    cover: require('assets/images/splash.png'),
-    //   icons
-    iconBack: require('assets/images/icons/back.png'),
-    iconFacebook: require('assets/images/icons/facebook.png'),
-    iconTwitter: require('assets/images/icons/twitter.png'),
-    iconGoogle: require('assets/images/icons/google.png'),
-    iconMenu: require('assets/images/icons/menu.png'),
-    iconMenuOpen: require('assets/images/icons/menu-open.png'),
-    iconCart: require('assets/images/icons/cart.png'),
-    iconSearch: require('assets/images/icons/search.png'),
-    iconVoice: require('assets/images/icons/voice.png'),
-    iconHome: require('assets/images/icons/home.png'),
-    iconHeart: require('assets/images/icons/heart.png'),
-    iconHeartLiked: require('assets/images/icons/heart-liked.png'),
-    iconBag: require('assets/images/icons/bag.png'),
-    iconWallet: require('assets/images/icons/wallet.png'),
-    iconSun: require('assets/images/icons/sun.png'),
-    iconAvatar: require('assets/images/icons/avatar.png'),
-    iconInfo: require('assets/images/icons/info.png'),
-    iconLogout: require('assets/images/icons/logout.png'),
-    iconBadge: require('assets/images/icons/badge.png'),
-    iconBagDrawer: require('assets/images/icons/bag-drawer.png'),
-    iconHeartDrawer: require('assets/images/icons/heart-drawer.png'),
-    iconWalletDrawer: require('assets/images/icons/wallet-drawer.png'),
-    iconClock: require('assets/images/icons/clock.png'),
-    reviewerAvatar: require('assets/images/icons/reviewer.png'),
-    ratingStart: require('assets/images/icons/start-rate.png')
-  };
-  
-  export default IMAGES;
-  
+  // icons
+  iconBack: require('assets/images/icons/back.webp'),
+  iconFacebook: require('assets/images/icons/facebook.webp'),
+  iconTwitter: require('assets/images/icons/twitter.webp'),
+  iconGoogle: require('assets/images/icons/google.webp'),
+  iconMenu: require('assets/images/icons/menu.webp'),
+  iconMenuOpen: require('assets/images/icons/menu-open.webp'),
+  iconCart: require('assets/images/icons/cart.webp'),
+  iconSearch: require('assets/images/icons/search.webp'),
+  iconVoice: require('assets/images/icons/voice.webp'),
+  iconHome: require('assets/images/icons/home.webp'),
+  iconHeart: require('assets/images/icons/heart.webp'),
+  iconHeartLiked: require('assets/images/icons/heart-liked.webp'),
+  iconBag: require('assets/images/icons/bag.webp'),
+  iconWallet: require('assets/images/icons/wallet.webp'),
+  iconSun: require('assets/images/icons/sun.webp'),
+  iconAvatar: require('assets/images/icons/avatar.webp'),
+  iconInfo: require('assets/images/icons/info.webp'),
+  iconLogout: require('assets/images/icons/logout.webp'),
+  iconBadge: require('assets/images/icons/badge.webp'),
+  iconBagDrawer: require('assets/images/icons/bag-drawer.webp'),
+  iconHeartDrawer: require('assets/images/icons/heart-drawer.webp'),
+  iconWalletDrawer: require('assets/images/icons/wallet-drawer.webp'),
+  iconClock: require('assets/images/icons/clock.webp'),
+  reviewerAvatar: require('assets/images/icons/reviewer.webp'),
+  ratingStart: require('assets/images/icons/start-rate.webp')
+};
+
+export default IMAGES;
