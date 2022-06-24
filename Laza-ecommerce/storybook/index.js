e4cfd90347e28696bcea0172b64e6e9f93500931
quangpham-agilityio <quang.pham@asnet.com.vn>
2022-06-24 10:51:33 +0700

    Remove Fonts unused

--- Laza-ecommerce/storybook/index.js
+++ Laza-ecommerce/storybook/index.js
@@ -7,7 +7,6 @@ import * as Font from 'expo-font';
 
 // load font
 Font.loadAsync({
-  Inter_Bold: require('assets/fonts/Inter-Bold.ttf'),
   Inter_400Regular: require('assets/fonts/Inter-Regular.ttf'),
   Inter_500Medium: require('assets/fonts/Inter-Medium.ttf'),
   Inter_600SemiBold: require('assets/fonts/Inter-SemiBold.ttf')
