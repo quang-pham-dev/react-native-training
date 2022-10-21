f1a018bb95a7ca82097def43a9d49c7ec8b80c9f
quangpham-agilityio <quang.pham@asnet.com.vn>
2022-10-21 11:10:34 +0700

    Add icons and images from design

--- src/themes/Images.ts
+++ src/themes/Images.ts
@@ -1,7 +1,33 @@
 // Icons
-const Icons = {}
+const Icons = {
+  back: require('@assets/icons/back.webp'),
+  facebook: require('@assets/icons/facebook.webp'),
+  twitter: require('@assets/icons/twitter.webp'),
+  google: require('@assets/icons/google.webp'),
+  menu: require('@assets/icons/menu.webp'),
+  menuOpen: require('@assets/icons/menu-open.webp'),
+  cart: require('@assets/icons/cart.webp'),
+  search: require('@assets/icons/search.webp'),
+  voice: require('@assets/icons/voice.webp'),
+  home: require('@assets/icons/home.webp'),
+  heart: require('@assets/icons/heart.webp'),
+  heartLiked: require('@assets/icons/heart-liked.webp'),
+  bag: require('@assets/icons/bag.webp'),
+  wallet: require('@assets/icons/wallet.webp'),
+  sun: require('@assets/icons/sun.webp'),
+  info: require('@assets/icons/info.webp'),
+  logout: require('@assets/icons/logout.webp'),
+  badge: require('@assets/icons/badge.webp'),
+  bagDrawer: require('@assets/icons/bag-drawer.webp'),
+  heartDrawer: require('@assets/icons/heart-drawer.webp'),
+  walletDrawer: require('@assets/icons/wallet-drawer.webp'),
+  clock: require('@assets/icons/clock.webp'),
+  ratingStart: require('@assets/icons/start-rate.webp'),
+}
 
 // Images
-const Images = {}
+const Images = {
+  splash: require('@assets/images/splash.png'),
+}
 
 export {Icons, Images}
