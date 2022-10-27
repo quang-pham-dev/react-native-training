e4c689fa18da73a3a8e594144581f74297968e94
quangpn <quangpn.dut@gmail.com>
2022-10-27 13:48:08 +0700

    Add data mock

---
+++ src/constants/dataMock/brands.ts
@@ -0,0 +1,33 @@
+export const brand = {
+  id: '0',
+  name: 'Nike',
+  url: 'https://1000logos.net/wp-content/uploads/2021/11/Nike-Logo.png',
+}
+
+export const brands = [
+  {
+    id: '0',
+    name: 'Nike',
+    url: 'https://1000logos.net/wp-content/uploads/2021/11/Nike-Logo.png',
+  },
+  {
+    id: '1',
+    name: 'Adidas',
+    url: 'https://www.pikpng.com/pngl/m/42-429866_adidas-logo-png-images-free-download-rh-pngimg.png',
+  },
+  {
+    id: '2',
+    name: 'Fila',
+    url: 'https://toppng.com/uploads/preview/fila-logo-vector-download-free-11574216707jcmvfu58fy.png',
+  },
+  {
+    id: '3',
+    name: 'Puma',
+    url: 'https://banner2.cleanpng.com/20180613/gsj/kisspng-tracksuit-puma-logo-brand-clothing-puma-logo-5b20dc6b232426.487874081528880235144.jpg',
+  },
+  {
+    id: '4',
+    name: 'Gucci',
+    url: 'https://esquilo.io/png/thumb/XBwczcMncYeff65-Vector-Gucci-Logo-PNG-Free-Download.png',
+  },
+]
---
+++ src/constants/dataMock/index.ts
@@ -0,0 +1,4 @@
+import {user} from './user'
+import {brand, brands} from './brands'
+
+export {brand, brands, user}
