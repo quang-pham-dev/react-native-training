929e005837dcddc03c705e7e5199402631566d13
quangpn <quangpn.dut@gmail.com>
2022-11-01 11:04:26 +0700

    Implement Dummy screen

---
+++ src/screens/Dummy/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// Styles
+import FlexStyled from '@components/Flex/Flex.styles'
+import PStyled from '@components/Paragraph/P.styles'
+
+const Dummy = () => {
+  return (
+    <FlexStyled.Center flex={1}>
+      <PStyled.Center>APP VERSION NOT YET DEVELOPED</PStyled.Center>
+    </FlexStyled.Center>
+  )
+}
+
+export default Dummy
